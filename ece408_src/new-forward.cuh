63c3b045d74159814d95130bdd4a878475e8e58d
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-04-15 23:44:02 -0500

    x_shared

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -10,7 +10,12 @@ namespace op
 {
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
-{
+{   
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define x_shared3d(c,w,h) x_shared[c*(TWK)*(TWK)+w*(TWK)+h]
+#define TWK (TILE_WIDTH+K)
     /*
     Modify this function to implement the forward pass described in Chapter 16.
     We have added an additional dimension to the tensors to support an entire mini-batch
@@ -18,48 +23,94 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
 
-    __shared__ float k_shared[1000];
-    int b,m,h,w,c,p,q;
+    extern __shared__ float k_shared[];
+    float* x_shared = k_shared+ C*K*K;
+    int b,m,h,w,c,p,q,tx,ty;
+    
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+    int W_grid = ceil(W_out / (TILE_WIDTH*1.0));
+    h = (blockIdx.z / W_grid)* TILE_WIDTH + threadIdx.y;
+    w = (blockIdx.z % W_grid)* TILE_WIDTH + threadIdx.x;
+    
+
     b = blockIdx.x;
     m = blockIdx.y;
-    int cursor = threadIdx.y*TILE_WIDTH+threadIdx.x;
+    tx = threadIdx.x;
+    ty = threadIdx.y;
+    //shared k
+    int cursor = ty*TILE_WIDTH+tx;
     while(cursor<C*K*K){
         k_shared[cursor]=k[m*C*K*K+cursor];
         cursor+=TILE_WIDTH*TILE_WIDTH;
     }
+
+    for (int c = 0; c < C; ++c){
+        int s1 = threadIdx.x;
+        while( s1 < TWK){
+            int s2 = threadIdx.y;
+            while( s2 < TWK){
+                if (w < W_out && h < H_out) x_shared[c*TWK*TWK+ s1* TWK + s2] = x4d(b,c, h+s1, w + s2);
+                s2 += TILE_WIDTH;
+            }           
+            s1 += TILE_WIDTH;
+        }
+    }
+
+
+
+    // //load upper left
+    // if(b*TILE_WIDTH+tx<W && m*TILE_WIDTH+ty<H){
+    //     for(int c=0; c<C; ++c){
+    //         x_shared3d(c,tx,ty)= x4d(b,c,tx,ty);
+    //     }
+    // }
+    // //load upper right
+    // int tmpx= tx+TILE_WIDTH;
+    // if(b*TILE_WIDTH+tmpx<W+K && m*TILE_WIDTH+ty<H){
+    //     for(int c=0; c<C; ++c){
+    //         x_shared3d(c,tmpx,ty)= x4d(b,c,tmpx,ty);
+    //     }
+    // }
+    // // load lower left
+    // int tmpy= ty+TILE_WIDTH;
+    // if(b*TILE_WIDTH+tx<W && m*TILE_WIDTH+tmpy<H+K){
+    //     for(int c=0; c<C; ++c){
+    //         x_shared3d(c,tx,tmpy)= x4d(b,c,tx,tmpy);
+    //     }
+    // }
+    // // load lower right
+
     __syncthreads();
-    
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    // two lines deleted 
+
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
 // y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
 /* my code starts here */
 
 /* my code ends here*/
-    int W_grid = ceil(W_out / (TILE_WIDTH*1.0));
-    h = (blockIdx.z / W_grid)* TILE_WIDTH + threadIdx.y;
-    w = (blockIdx.z % W_grid)* TILE_WIDTH + threadIdx.x;
+
 
     /*The following part is not parallelizable*/
     float sum = 0.0;
-    for(c = 0; c<C; ++c){
-        for(p=0; p<K; ++p){
-            for(q =0; q<K; ++q){
-                    if(w<W_out && h<H_out)
-                        sum+= x4d(b,c,h+p,w+q)*k_shared[c*K*K+p*K + q];
+    if(w<W_out && h<H_out){
+        for(c = 0; c<C; ++c){
+            for(p=0; p<K; ++p){
+                for(q =0; q<K; ++q){
+                            sum+= x_shared3d(c,h+p,w+q)*k_shared[c*K*K+p*K + q];
+                }
             }
         }
-    }
-    if(w<W_out && h<H_out)
         y4d(b,m,h,w) =sum;
+    }
+        
 #undef y4d
 #undef x4d
 #undef k4d
+#undef x_shared3d
+#undef TWK
 }
 
 /* 
@@ -95,7 +146,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
 
     // Call the kernel
-    forward_kernel<<<gridDim,blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim,blockDim, sizeof(float)*(C*K*K+C*(TILE_WIDTH+K)*(TILE_WIDTH+K))>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
