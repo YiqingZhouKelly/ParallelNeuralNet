b1e39a15b58ee40564992dbe54af34c506cc9bf9
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-04-21 17:02:04 -0500

    fix compilation error, illegal mem access in MM kernel

--- ece408_src/new-forward-unroll.cuh
+++ ece408_src/new-forward-unroll.cuh
@@ -11,8 +11,8 @@ namespace mxnet
 {
 namespace op
 {
-__constant__ float k_const[2400];
-// __constant__ float unrolled_k_const[2400];
+// __constant__ float k_const[2400];
+__constant__ float unrolled_k_const[2400];
 
 
 __global__ void unrollx_kernel(int C, int K,int H, int W, const float* x, float* unroll_x){
@@ -34,7 +34,7 @@ __global__ void unrollx_kernel(int C, int K,int H, int W, const float* x, float*
     if(posy<H && posx<W){
         int in = x4d(b,c,posy,posx);
         for(int p=0; p<K; ++p){
-            for int(q=0; q<K; ++q){ // (p,q) = location in the filter
+            for(int q=0; q<K; ++q){ // (p,q) = location in the filter
                 int filterStartx= posx-q;
                 int filterStarty= posy-p;
                 if(filterStarty>=0 && filterStartx>=0){
@@ -78,17 +78,10 @@ __global__ void unrollx_kernel(int C, int K,int H, int W, const float* x, float*
 //     #undef unroll_k3d
 // }
 
-__global__ void forward_kernel_unrolling(float *y, const float *unrolled_x, const float *unrolled_k, 
-                               const int B, const int M, const int C, 
-                               const int H, const int W, const int K)
-{   
-    // unrollx_kernel(int C, int K,int H, int W, const float* x, float* unroll_x);
-}
 
 
 
-
-__global__ void matrixMultiplyShared(float *A, float *unrolled_x, float *C,
+__global__ void matrixMultiply(const float *A, const float *unrolled_x, float *C,
                                      int numARows, int numAColumns,
                                      int numBRows, int numBColumns,
                                      int numCRows, int numCColumns,
@@ -98,9 +91,10 @@ __global__ void matrixMultiplyShared(float *A, float *unrolled_x, float *C,
      * significant dim is B, which is mapped to threadidx.z, so do not need to explicitly take care
      * of it during computation, but you do need to be careful when read/ write to 3D arrays.
      */
-      float* B = unrolled_x+ blockIdx.z* (numBColumns*numBRows); // b = blockIdx.z
-      __shared__ float subTileM[32][32];
-      __shared__ float subTileN[32][32];
+  // A is already in constant memory
+      const float* B = unrolled_x+ blockIdx.z* (numBColumns*numBRows); // b = blockIdx.z
+      // __shared__ float subTileM[32][32];
+      __shared__ float subTileN[8][8];
       int bx = blockIdx.x;  int by = blockIdx.y;
       int tx = threadIdx.x; int ty = threadIdx.y;
 
@@ -114,30 +108,35 @@ __global__ void matrixMultiplyShared(float *A, float *unrolled_x, float *C,
       if (numAColumns > Width) Width = numAColumns;
       
       for (int m = 0; m < Width/TILE_WIDTH + 1; ++m) {
-           // Collaborative loading of M and N tiles into shared memory
-        if (Row < numARows && (m*TILE_WIDTH+tx) < numAColumns) 
-          subTileM[ty][tx] = A[Row*numAColumns + m*TILE_WIDTH+tx];
-        else 
-          subTileM[ty][tx] = 0;
+        // Collaborative loading of M and N tiles into shared memory
+        // if (Row < numARows && (m*TILE_WIDTH+tx) < numAColumns) 
+        //   subTileM[ty][tx] = A[Row*numAColumns + m*TILE_WIDTH+tx];
+        // else 
+        //   subTileM[ty][tx] = 0;
         if (Col < numBColumns && (m*TILE_WIDTH+ty) < numBRows)
-          subTileN[ty][tx] = B[(m*TILE_WIDTH+ty)*numBColumns+Col];
+          subTileN[ty][tx] = B[threadIdx.z*numBColumns*numBRows+(m*TILE_WIDTH+ty)*numBColumns+Col];
         else
           subTileN[ty][tx] = 0;
         __syncthreads();
         for (int k = 0; k < TILE_WIDTH; ++k)
-              Pvalue += subTileM[ty][k] * subTileN[k][tx];
+              Pvalue += /*subTileM[ty][k]*/A[Row*numAColumns+m*TILE_WIDTH+k] * subTileN[k][tx];
         __syncthreads();
      }  
      //numCRows =M
      #define C4d(b,m,y,x) C[(b)*(numCRows*H_out*W_out) +(m)*(H_out*W_out)+(y)*(W_out)+(x)]
-     if (Row < numCRows && Col < numCColumns) C3d(Row,Col/W_out, Col%W_out)=Pvalue;
-     //C[blockIdx.z*(numCColumns*numCRows)+Row*numCColumns+Col] = Pvalue;
+     if (Row < numCRows && Col < numCColumns && 
+      blockIdx.z*(numCColumns*numCRows)+Row*numCColumns+Col< 10000) {
+        // C4d(blockIdx.z,Row,Col/W_out, Col%W_out)=Pvalue;
+       C[Row*numCColumns+Col] = Pvalue; //?? illegal mem access? blockIdx.z*(numCColumns*numCRows)+
+
+      }
+      // C[blockIdx.z*(numCColumns*numCRows)+Row*numCColumns+Col] = Pvalue; //?? illegal mem access?
      #undef C4d
 }
 
 
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, 
+/*__global__ void forward_kernel(float *y, const float *x, const float *k, 
                                const int B, const int M, const int C, 
                                const int H, const int W, const int K)
 {   
@@ -192,7 +191,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k,
 #undef k4d
 #undef x_shared3d
 #undef TWK
-}
+}*/
 
 
 template <>
@@ -208,18 +207,19 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = w.shape_[3];
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
 
     int W_grid = ceil(W_out / (TILE_WIDTH*1.0));
     int H_grid = ceil(H_out / (TILE_WIDTH*1.0));
     int Z = H_grid * W_grid;
 
-    cudaMemcpyToSymbol(k_const,w.dptr_, sizeof(float)*C*K*K*M,0);
+    // cudaMemcpyToSymbol(k_const,w.dptr_, sizeof(float)*C*K*K*M,0);
     //*************************************//
     //cpu unrolling k
-    float* k = w.dptr_;
-    float* unrolled_k = malloc(sizeof(float)* M*C*K*K);
+    float* k = (float*)malloc(sizeof(float)* M*C*K*K);
+    cudaMemcpy(k, w.dptr_, sizeof(float)* M*C*K*K, cudaMemcpyDeviceToHost);
+    float* unrolled_k = (float*)malloc(sizeof(float)* M*C*K*K);
     for(int m=0; m<M; ++m){
         for(int c=0;c<C;++c){
             for(int p=0; p<K;++p){
@@ -229,16 +229,33 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
             }
         }
     }
-    cudaMemcpyToSymbol(unrolled_k_const,w.dptr_, sizeof(float)*C*K*K*M,0);
+    free(k);
+    cudaMemcpyToSymbol(unrolled_k_const,unrolled_k, sizeof(float)*C*K*K*M,0);
 
+    float* unroll_x;
+    cudaMalloc((void**)&unroll_x, sizeof(float) *B*C*K*K*H_out*W_out);
 
-    //***********************************//
-    dim3 gridDim(B,M,Z);
-    dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
-    forward_kernel<<<gridDim,blockDim, 
-                    sizeof(float)*(C*(TILE_WIDTH+K)*(TILE_WIDTH+K))>>>
-                    (y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    dim3 DimGrid(ceil(W*1.0/TILE_WIDTH), ceil(H*1.0/TILE_WIDTH), B*C);
+    dim3 DimBlock(TILE_WIDTH,TILE_WIDTH,1);
+    unrollx_kernel<<<DimGrid, DimBlock>>>(C,K,H,W,x.dptr_,unroll_x);
 
+    dim3 DimGridMM(ceil(M*1.0/TILE_WIDTH), ceil(H_out*W_out*1.0/TILE_WIDTH), B);
+    dim3 DimBlockMM(TILE_WIDTH,TILE_WIDTH,1);
+    matrixMultiply<<<DimGridMM, DimBlockMM>>>(unrolled_k_const,unroll_x, y.dptr_,
+                                               M, C*K*K, 
+                                               C*K*K,H_out*W_out,
+                                               M,H_out*W_out,
+                                               W_out,H_out);
+    // //***********************************//
+    // dim3 gridDim(B,M,Z);
+    // dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
+    // forward_kernel<<<gridDim,blockDim, 
+    //                 sizeof(float)*(C*(TILE_WIDTH+K)*(TILE_WIDTH+K))>>>
+    //                 (y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+
+    //**********************
+    cudaFree(unroll_x);
+    //************************
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
     #undef k4d
 }
